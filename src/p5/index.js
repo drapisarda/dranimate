e6572f6ee097d6b8c9eb463a1b7de2085bca0739
Daniele Rapisarda <info@danielerapisarda.com>
2021-10-12 22:28:15 +0200

    bolle

--- src/p5/index.js
+++ src/p5/index.js
@@ -1,6 +1,5 @@
 import './style.scss';
 import p5 from 'p5';
-import createLoop from 'p5.createloop';
 
 const xPoints = 70;
 const yPoints = 70;
@@ -9,30 +8,35 @@ let stepY;
 let minDistX;
 let minDistY;
 let pointSize = 5;
-const noiseRate = 0.002;
+const noiseScale = 0.1;
+let a = 0;
 
 const sketch = (s) => {
   s.setup = () => {
-    s.createCanvas(s.windowWidth/2, s.windowHeight/2);
+    // s.createCanvas(Math.max(500, s.windowWith), Math.max(500, s.windowHeight));
+    s.createCanvas(500, 500);
     stepX = s.floor(s.width / xPoints);
     stepY = s.floor(s.height / yPoints);
     // minDistX = stepX/2;
     // minDistY = stepY/2;
     s.frameRate(10)
     // pointsDistance = (stepX**2 + stepY**2)**0.5;
-    console.log(typeof createLoop)
-    // createLoop({ duration: 15, gif: true })
   }
 
   s.draw = () => {
     s.background(0);
-    const { mouseX, mouseY, frameCount} = s;
-    
+    const { mouseX, mouseY, frameCount, ceil, lerp} = s;
+    // const mark = ceil(s.noise(frameCount * noiseRate * s.random()) * xPoints);
     for (let i = 0; i < xPoints; i++) {
+      a += s.radians(noiseScale);
+      const noise = s.noise(s.cos(a), s.sin(a));
+      const mark = lerp(0, xPoints, noise);
+      if (frameCount < 10) console.log(noise)
+      
       for (let j = 0; j < yPoints; j++) {
-        const mark = Math.ceil(s.noise(Math.sin(frameCount * i * noiseRate), Math.sin(frameCount * j * noiseRate)) * yPoints);
+        // const mark = s.noise(Math.sin(frameCount * i * noiseRate), Math.sin(frameCount * j * noiseRate)) * yPoints;
         // const fillColor = Math.abs(( xPos * s.noise(xPos) + yPos * s.noise(yPos) + s.frameCount * animationRate) % 256);
-        const fillColor = Math.abs(i - mark) / xPoints * 256;
+        const fillColor = Math.abs(j - mark) / xPoints * 255 *2;
         s.fill(fillColor);
 
         let xPos = i * stepX + pointSize / 2;
